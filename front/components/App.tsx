73dc3d36041b475ff21bf86ddc3dc78e0ad1cf32
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2021-07-25 19:40:58 +0900

    登録フォーム復活

--- front/components/App.tsx
+++ front/components/App.tsx
@@ -6,9 +6,8 @@ import { PlayContainer } from './containers/Play';
 import { MyPage } from './presentationals/pages/MyPage';
 import { SigninContainer } from '../components/containers/Signin';
 import { Signup } from './presentationals/pages/Signup';
-// import { RegistTrainingsContaniner } from './containers/RegistTrainings';
+import { RegistTrainingsContaniner } from './containers/RegistTrainings';
 import { DetailTrainingContaniner } from './containers/DetailTraining';
-import { RegistTrainings } from './presentationals/pages/RegistTrainings';
 
 export interface AppProps {
   user?: { user_id: string, name: string },
@@ -31,7 +30,8 @@ export const App: FC<AppProps> = ({
           (
             <Menu.Menu position='right'>
               <Menu.Item as={Link} to='/regist/trainings'>Regist</Menu.Item>
-              <Menu.Item as={Link} to='/mypage'>MyPage</Menu.Item>
+              {/* 未実装 */}
+              {/* <Menu.Item as={Link} to='/mypage'>MyPage</Menu.Item> */}
               <Menu.Item onClick={handleLogout}>Logout</Menu.Item>
             </Menu.Menu>
           )
@@ -49,8 +49,7 @@ export const App: FC<AppProps> = ({
           <Route path='/' exact component={HomeContainer} />
           <Route path='/play/category/:categoryId/total/:totalTrainingTime/per_time/:trainingTime/per_break/:breakTime' component={PlayContainer} />
           <Route path='/regist/trainings'>
-            {/* {!isLoggedIn ? <Redirect to="/signin" /> : <RegistTrainingsContaniner userId={user.user_id} />} */}
-            <RegistTrainings />
+            {isLoggedIn ? <RegistTrainingsContaniner userId={user.user_id} /> : <Redirect to="/signin" />}
           </Route>
           <Route path='/detail/training/:categoryId'>
             <DetailTrainingContaniner isLoggedIn={isLoggedIn} userId={user.user_id} />
